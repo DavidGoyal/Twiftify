b2b5b418c5734a99f05d35595661670f587cbecf
David <goyaldavid55@gmail.com>
2024-11-28 22:19:50 +0530

    Added token to callback

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,7 +15,7 @@ const Header = async () => {
 	}
 
 	const session = await auth();
-	console.log(session);
+	console.log(session?.user.token);
 
 	return (
 		<header className="w-full h-[6rem] flex justify-between items-center p-6 pb-2">
