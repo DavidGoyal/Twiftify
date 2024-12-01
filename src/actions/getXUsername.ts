dd751fe1d56b706a1afa51ca99bd2b47cd119449
David <goyaldavid55@gmail.com>
2024-12-01 12:28:05 +0530

    Fixed query param

--- src/actions/getXUsername.ts
+++ src/actions/getXUsername.ts
@@ -2,15 +2,12 @@
 
 export const getXUsername = async (token: string) => {
 	try {
-		const response = await fetch(
-			"https://api.x.com/2/users/me?query=from:twitterdev",
-			{
-				headers: {
-					Authorization: `Bearer ${token}`,
-					"User-Agent": "v2UserLookupJS",
-				},
-			}
-		);
+		const response = await fetch("https://api.x.com/2/users/me", {
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"User-Agent": "v2UserLookupJS",
+			},
+		});
 		const data = await response.json();
 		console.log(data);
 		return data?.username || null;
