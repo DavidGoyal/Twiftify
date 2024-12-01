0438488ced24a3da905d11e76fe5b7142f5cc287
David <goyaldavid55@gmail.com>
2024-12-01 12:24:17 +0530

    Added try catch

--- src/actions/getXUsername.ts
+++ src/actions/getXUsername.ts
@@ -1,16 +1,21 @@
 "use server";
 
 export const getXUsername = async (token: string) => {
-	const response = await fetch(
-		"https://api.x.com/2/users/me?query=from:twitterdev",
-		{
-			headers: {
-				Authorization: `Bearer ${token}`,
-				"User-Agent": "v2UserLookupJS",
-			},
-		}
-	);
-	const data = await response.json();
-	console.log(data);
-	return data?.username || null;
+	try {
+		const response = await fetch(
+			"https://api.x.com/2/users/me?query=from:twitterdev",
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"User-Agent": "v2UserLookupJS",
+				},
+			}
+		);
+		const data = await response.json();
+		console.log(data);
+		return data?.username || null;
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
 };
